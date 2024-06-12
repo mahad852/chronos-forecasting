172ec4ea2eb545bf2164b1122342c7005541e5cf
Mahad <mahad852@gmail.com>
2024-06-12 06:56:50 +0500

    added script to test fine tuned model on ecg data with splits

---
+++ test_ecg_splits.py
@@ -0,0 +1,83 @@
+import numpy as np
+from chronos import ChronosPipeline
+import os
+import torch
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+context_len = 512
+pred_len = 64
+
+data_path = "/home/mali2/datasets/ecg/MIT-BIH-splits.npz"
+
+model_path = "scripts/output/run-8/checkpoint-final/"
+
+pipeline = ChronosPipeline.from_pretrained(
+    model_path,
+    device_map="cuda:0",  # use "cpu" for CPU inference and "mps" for Apple Silicon
+    torch_dtype=torch.bfloat16,
+)
+
+def single_loader(dataset):
+    for i in range(1, len(dataset.files), 2):
+        xy = dataset[dataset.files[i]]
+        x, y = xy[:context_len], xy[context_len:context_len + pred_len]
+
+        yield [x], [y]
+
+dataset = np.load(data_path)
+
+mses = []
+maes = []
+
+mse_by_pred_len = {}
+rmse_by_pred_len = {}
+mae_by_pred_len = {}
+
+total = 0
+
+for p_len in range(1, pred_len + 1):
+    mse_by_pred_len[p_len] = 0.0
+    rmse_by_pred_len[p_len] = 0.0
+    mae_by_pred_len[p_len] = 0.0
+
+for i, (x, y) in enumerate(single_loader(dataset)):
+    forecast = pipeline.predict(
+        context=torch.tensor(x),
+        prediction_length=pred_len,
+        num_samples=20,
+    )
+    y = np.array(y)
+    forecast = np.quantile(forecast.numpy(), 0.5, axis=1)
+
+    mse = mean_squared_error(y, forecast)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y, forecast)
+
+    mses.append(mse)
+    maes.append(mae)
+
+    total += 1
+
+    for p_len in range(1, pred_len + 1):
+        mse_by_pred_len[p_len] += mean_squared_error(y, forecast)
+        mae_by_pred_len[p_len] += mean_absolute_error(y, forecast)
+
+    if i % 20 == 0:
+        print(f"iteraition: {i} | MSE: {mse} RMSE: {rmse} MAE: {mae}")
+
+print(f"MSE: {np.average(mses)} RMSE: {np.sqrt(np.average(mses))} MAE: {np.average(maes)}")
+
+for p_len in range(1, pred_len + 1):
+    mse_by_pred_len[p_len] /= total
+    rmse_by_pred_len[p_len] = np.sqrt(mse_by_pred_len[p_len])
+    mae_by_pred_len[p_len] /= total
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+with open(os.path.join("logs", f"Chronos_Mini_Run8_{context_len}_{pred_len}.csv"), "w") as f:
+    f.write("context_len,horizon_len,MSE,RMSE,MAE\n")
+    for p_len in range(1, pred_len + 1):
+        f.write(f"{context_len},{p_len},{mse_by_pred_len[p_len]},{rmse_by_pred_len[p_len]},{mae_by_pred_len[p_len]}")
+        if p_len != pred_len:
+            f.write("\n")
