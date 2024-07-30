b21b80327d1bdd323fe84c7f09d29e4d3a76433a
Mahad <mahad852@gmail.com>
2024-07-31 04:09:20 +0500

    added vital_signs arrow dataset creation script and train function

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -639,6 +639,188 @@ def main(
         )
 
 
+def train_vital_signs(
+    training_data_paths: List[str],
+    probability: Optional[float] = 1.0,
+    context_length: int = 512,
+    prediction_length: int = 64,
+    min_past: int = 64,
+    max_steps: int = 200_000,
+    save_steps: int = 50_000,
+    log_steps: int = 500,
+    per_device_train_batch_size: int = 32,
+    learning_rate: float = 1e-3,
+    optim: str = "adamw_torch_fused",
+    shuffle_buffer_length: int = 100,
+    gradient_accumulation_steps: int = 2,
+    model_id: str = "google/t5-efficient-tiny",
+    model_type: str = "seq2seq",
+    random_init: bool = False,
+    tie_embeddings: bool = False,
+    output_dir: str = "./output/",
+    tf32: bool = True,
+    torch_compile: bool = True,
+    tokenizer_class: str = "MeanScaleUniformBins",
+    n_tokens: int = 4096,
+    n_special_tokens: int = 2,
+    pad_token_id: int = 0,
+    eos_token_id: int = 1,
+    use_eos_token: bool = True,
+    lr_scheduler_type: str = "linear",
+    warmup_ratio: float = 0.0,
+    dataloader_num_workers: int = 1,
+    max_missing_prop: float = 0.9,
+    num_samples: int = 20,
+    temperature: float = 1.0,
+    top_k: int = 50,
+    top_p: float = 1.0,
+    seed: Optional[int] = None,
+):
+    if tf32 and not (
+        torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8
+    ):
+        # TF32 floating point format is available only on NVIDIA GPUs
+        # with compute capability 8 and above. See link for details.
+        # https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capability-8-x
+        log_on_main(
+            "TF32 format is only available on devices with compute capability >= 8. "
+            "Setting tf32 to False.",
+            logger,
+        )
+        tf32 = False
+
+    if seed is None:
+        seed = random.randint(0, 2**32)
+
+    log_on_main(f"Using SEED: {seed}", logger)
+    transformers.set_seed(seed=seed)
+
+    raw_training_config = deepcopy(locals())
+    output_dir = Path(output_dir)
+
+    assert isinstance(training_data_paths, list)
+
+    assert isinstance(probability, list)
+    assert len(probability) == len(training_data_paths)
+
+
+    tokenizer_kwargs: str = {"low_limit": -15.0, "high_limit": 15.0}
+    
+    assert model_type in ["seq2seq", "causal"]
+
+    if not model_type == "seq2seq":
+        raise NotImplementedError("Only seq2seq models are currently supported")
+
+    output_dir = get_next_path("run", base_dir=output_dir, file_type="")
+
+    log_on_main(f"Logging dir: {output_dir}", logger)
+    log_on_main(
+        f"Loading and filtering {len(training_data_paths)} datasets "
+        f"for training: {training_data_paths}",
+        logger,
+    )
+
+    log_on_main(
+        f"Mixing probabilities: {probability}",
+        logger,
+    )
+
+    train_datasets = [
+        Filter(
+            partial(
+                has_enough_observations,
+                min_length=min_past + prediction_length,
+                max_missing_prop=max_missing_prop,
+            ),
+            FileDataset(path=Path(data_path), freq="h"),
+        )
+        for data_path in training_data_paths
+    ]
+
+    log_on_main("Initializing model", logger)
+
+    model = load_model(
+        model_id=model_id,
+        model_type=model_type,
+        vocab_size=n_tokens,
+        random_init=random_init,
+        tie_embeddings=tie_embeddings,
+        pad_token_id=pad_token_id,
+        eos_token_id=eos_token_id,
+    )
+
+    chronos_config = ChronosConfig(
+        tokenizer_class=tokenizer_class,
+        tokenizer_kwargs=tokenizer_kwargs,
+        n_tokens=n_tokens,
+        n_special_tokens=n_special_tokens,
+        pad_token_id=pad_token_id,
+        eos_token_id=eos_token_id,
+        use_eos_token=use_eos_token,
+        model_type=model_type,
+        context_length=context_length,
+        prediction_length=prediction_length,
+        num_samples=num_samples,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+    )
+
+    # Add extra items to model config so that it's saved in the ckpt
+    model.config.chronos_config = chronos_config.__dict__
+
+    shuffled_train_dataset = ChronosDataset(
+        datasets=train_datasets,
+        probabilities=probability,
+        tokenizer=chronos_config.create_tokenizer(),
+        context_length=context_length,
+        prediction_length=prediction_length,
+        min_past=min_past,
+        mode="training",
+    ).shuffle(shuffle_buffer_length=shuffle_buffer_length)
+
+    # Define training args
+    training_args = TrainingArguments(
+        output_dir=str(output_dir),
+        per_device_train_batch_size=per_device_train_batch_size,
+        learning_rate=learning_rate,
+        lr_scheduler_type=lr_scheduler_type,
+        warmup_ratio=warmup_ratio,
+        optim=optim,
+        logging_dir=str(output_dir / "logs"),
+        logging_strategy="steps",
+        logging_steps=log_steps,
+        save_strategy="steps",
+        save_steps=save_steps,
+        report_to=["tensorboard"],
+        max_steps=max_steps,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        dataloader_num_workers=dataloader_num_workers,
+        tf32=tf32,  # remove this if not using Ampere GPUs (e.g., A100)
+        torch_compile=torch_compile,
+        ddp_find_unused_parameters=False,
+        remove_unused_columns=False,
+    )
+
+    # Create Trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=shuffled_train_dataset,
+    )
+    log_on_main("Training", logger)
+
+    trainer.train()
+
+    if is_main_process():
+        model.save_pretrained(output_dir / "checkpoint-final")
+        save_training_info(
+            output_dir / 
+            "checkpoint-final", training_config=raw_training_config
+        )
+    
+    return model
+
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     logger = logging.getLogger(__file__)
