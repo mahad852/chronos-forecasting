f0fc94b62bb717ad74fd2793a723dbfc1815f611
Mahad <mahad852@gmail.com>
2024-07-31 04:48:42 +0500

    fixing typo in train script (vital_signs)

--- train_vital_signs.py
+++ train_vital_signs.py
@@ -1,7 +1,7 @@
 from scripts.training.train import train_vital_signs
 import os
 
-if not os.path.exist("weights/client01"):
+if not os.path.exists("weights/client01"):
     os.makedirs("weights/client01")
 
 train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model_id="google/t5-efficient-mini", context_length=600, prediction_length=60, output_dir="weights/client01/")
