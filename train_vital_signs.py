11055e34076c4c50144b3e988dc40f5a4a250def
Mahad <mahad852@gmail.com>
2024-07-31 05:13:29 +0500

    using amazon-t5-mini

--- train_vital_signs.py
+++ train_vital_signs.py
@@ -4,4 +4,4 @@ import os
 if not os.path.exists("weights/client01"):
     os.makedirs("weights/client01")
 
-train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model_id="google/t5-efficient-mini", context_length=600, prediction_length=60, output_dir="weights/client01/")
+train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model_id="amazon/chronos-t5-mini", context_length=600, prediction_length=60, output_dir="weights/client01/")
