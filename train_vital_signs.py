5166b103739942a1120396409d08d1973b070eb3
Mahad <mahad852@gmail.com>
2024-07-31 06:12:53 +0500

    changed the max steps in train vital_signs to 10k

--- train_vital_signs.py
+++ train_vital_signs.py
@@ -4,4 +4,4 @@ import os
 if not os.path.exists("weights/client01"):
     os.makedirs("weights/client01")
 
-train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model_id="amazon/chronos-t5-mini", context_length=600, prediction_length=60, output_dir="weights/client01/")
+train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model_id="amazon/chronos-t5-tiny", context_length=600, prediction_length=60, output_dir="weights/client01/", max_steps=10000)
