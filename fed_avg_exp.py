2e5737d1ab3255818dd570f5a8a60581c7ba21e9
Mahad Ali <mahad852@gmail.com>
2024-08-09 06:22:02 -0400

    testing with max_steps=10

--- fed_avg_exp.py
+++ fed_avg_exp.py
@@ -126,7 +126,7 @@ class FlowerClient(NumPyClient):
         set_params(self.model, parameters)
 
         # do local training (call same function as centralised setting)
-        self.model = train_vital_signs(training_data_paths=[self.train_data_path], model=self.model, context_length=context_len, prediction_length=pred_len, max_steps=5000)
+        self.model = train_vital_signs(training_data_paths=[self.train_data_path], model=self.model, context_length=context_len, prediction_length=pred_len, max_steps=max_steps)
 
         # return the model parameters to the server as well as extra info (number of training examples in this case)
         return get_params(self.model), max_steps, {}
@@ -142,22 +142,13 @@ class FlowerClient(NumPyClient):
         return float(rmse), len(self.valloader), {"mae": mae, "mse": mse}
     
 
-# def client_fn(context: Context):
-#     """Returns a FlowerClient containing its data partition."""
-
-#     partition_id = int(context.node_config["partition-id"])
-
-#     return FlowerClient(train_data_path=f"vital_signs_arrow/client0{partition_id + 1}.arrow", 
-#                         valloader=DataLoader(client_ds[partition_id], batch_size=batch_size, shuffle=False)).to_client()
-
-def client_fn(cid : str):
+def client_fn(context: Context):
     """Returns a FlowerClient containing its data partition."""
 
-    partition_id = int(cid)
+    partition_id = int(context.node_config["partition-id"])
 
     return FlowerClient(train_data_path=f"vital_signs_arrow/client0{partition_id + 1}.arrow", 
                         valloader=DataLoader(client_ds[partition_id], batch_size=batch_size, shuffle=False)).to_client()
-
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -171,39 +162,6 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     return {"mae": sum(maes) / sum(examples), "mse": sum(mses) / sum(examples)}
 
 
-# Concstruct the ClientApp passing the client generation function
-client_app = ClientApp(client_fn=client_fn)
-
-def server_fn(context: Context):
-    # instantiate the model
-    model = load_model(model_id=model_path)
-    ndarrays = get_params(model)
-    # Convert model parameters to flwr.common.Parameters
-    global_model_init = ndarrays_to_parameters(ndarrays)
-
-    # Define the strategy
-    strategy = FedAvg(
-        fraction_fit=0.1,  # 10% clients sampled each round to do fit()
-        fraction_evaluate=0.5,  # 50% clients sample each round to do evaluate()
-        evaluate_metrics_aggregation_fn=weighted_average,  # callback defined earlier
-        initial_parameters=global_model_init,  # initialised global model
-    )
-
-    # Construct ServerConfig
-    config = ServerConfig(num_rounds=num_rounds)
-
-    # Wrap everything into a `ServerAppComponents` object
-    return ServerAppComponents(strategy=strategy, config=config)
-
-# Create your ServerApp
-server_app = ServerApp(server_fn=server_fn)
-
-
-# run_simulation(
-#     server_app=server_app, client_app=client_app, num_supernodes=3
-# )
-
-
 model = load_model(model_id=model_path)
 ndarrays = get_params(model)
 global_model_init = ndarrays_to_parameters(ndarrays)
@@ -215,7 +173,7 @@ strategy = FedAvg(
 )
 
 # each client gets 1xCPU (this is the default if no resources are specified)
-my_client_resources = {'num_cpus': 1, 'num_gpus': 0.5}
+my_client_resources = {'num_cpus': 1, 'num_gpus': 1/3}
 
 
 start_simulation(
