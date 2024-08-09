269b674f497b03b43d695cbb90a88ff2d86639a1
Mahad Ali <mahad852@gmail.com>
2024-08-09 04:51:05 -0400

    potential fix for train_vital_signs error

--- fed_avg_exp.py
+++ fed_avg_exp.py
@@ -1,5 +1,5 @@
 from chronos import ChronosPipeline
-from scripts.training.train import train_vital_signs
+from scripts.training.train import train_vital_signs, load_model
 from collections import OrderedDict
 from typing import Dict, Tuple
 
@@ -28,13 +28,13 @@ def get_params(model):
     return [val.cpu().numpy() for _, val in model.state_dict().items()]
 
 
-def test(model, val_loader):
+def test(pipeline: ChronosPipeline, val_loader):
     mses = []
     rmses = []
     maes = []
 
     for _, (x, y) in enumerate(val_loader):
-        forecast = model.predict(
+        forecast = pipeline.predict(
             context=x,
             prediction_length=pred_len,
             num_samples=20,
@@ -58,7 +58,7 @@ class FlowerClient(NumPyClient):
 
         self.train_data_path = train_data_path
         self.valloader = valloader
-        self.model = ChronosPipeline.from_pretrained(
+        self.pipeline = ChronosPipeline.from_pretrained(
             model_path,
             device_map="cuda",  # use "cpu" for CPU inference and "mps" for Apple Silicon
             torch_dtype=torch.bfloat16,
@@ -70,21 +70,21 @@ class FlowerClient(NumPyClient):
         of the locally trained model are communicated back to the server"""
 
         # copy parameters sent by the server into client's local model
-        set_params(self.model, parameters)
+        set_params(self.pipeline.model, parameters)
 
         # do local training (call same function as centralised setting)
-        self.model = train_vital_signs(training_data_paths=[self.train_data_path], model=self.model, context_length=context_len, prediction_length=pred_len, max_steps=5000)
+        self.pipeline.model = train_vital_signs(training_data_paths=[self.train_data_path], model=self.model, context_length=context_len, prediction_length=pred_len, max_steps=5000)
 
         # return the model parameters to the server as well as extra info (number of training examples in this case)
-        return get_params(self.model), 5000, {}
+        return get_params(self.pipeline.model), 5000, {}
 
     def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
         """Evaluate the model sent by the server on this client's
         local validation set. Then return performance metrics."""
 
-        set_params(self.model, parameters)
+        set_params(self.pipeline.model, parameters)
         # do local evaluation (call same function as centralised setting)
-        loss, accuracy = test(self.model, self.valloader)
+        loss, accuracy = test(self.pipeline, self.valloader)
         # send statistics back to the server
         return float(loss), len(self.valloader), {"accuracy": accuracy}
 
@@ -93,6 +93,8 @@ pipeline = ChronosPipeline.from_pretrained(
     device_map="cuda",  # use "cpu" for CPU inference and "mps" for Apple Silicon
     torch_dtype=torch.bfloat16,
 )
+
+pipeline.model = load_model(model_id=model_path)
 
 model = train_vital_signs(training_data_paths=["vital_signs_arrow/client01.arrow"], model=pipeline.model, context_length=context_len, prediction_length=pred_len, output_dir="weights/client01/", max_steps=10)
 
