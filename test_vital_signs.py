attempting to fix smape calculation

test_vital_signs.py
@@ -18,7 +18,8 @@ batches = 30000
 data_path = "/home/mali2/datasets/vital_signs" # "/home/mali2/datasets/vital_signs" # "/Users/ma649596/Downloads/vital_signs_data/data"
 
 def calculate_smape(y_gt, y_pred):
-    return 100/len(y_gt) * np.sum(2 * np.abs(y_pred - y_gt) / (np.abs(y_gt) + np.abs(y_pred) + 1e-9))
+    return np.mean(200 * np.abs(y_pred - y_gt) / (np.abs(y_pred) + np.abs(y_gt) + 1e-8))
+    # return 100/len(y_gt) * np.sum(2 * np.abs(y_pred - y_gt) / (np.abs(y_gt) + np.abs(y_pred) + 1e-9))
 
 print(f"Data path: {data_path}. Loading vital signs data...")
 