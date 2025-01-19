2cc2fa9e4e1f8b2118e4ed9bb1501fe92609ff05
mahad852 <mahad852@gmail.com>
2025-01-19 15:36:56 -0500

    testing new scaffold model

--- test_vital_signs.py
+++ test_vital_signs.py
@@ -70,7 +70,7 @@ pipeline = ChronosPipeline.from_pretrained(
 ###############################################################################################################################
 ###############################################################################################################################
 
-npy_model = np.load("logs/fedavg_iid/round-9-weights.npz")
+npy_model = np.load("logs/scaffold_iid/round-10-weights.npz")
 npy_params = [npy_model[file] for file in npy_model.files]
 set_params(pipeline.model, npy_params)
 
@@ -160,8 +160,9 @@ if not os.path.exists("logs"):
 # with open(os.path.join("logs", f"Chronos_Tiny_CenBoth_Resting_{context_len}_{pred_len}.csv"), "w") as f:
 # with open(os.path.join("logs", f"Chronos_Tiny_CenEcgHalf_Resting_{context_len}_{pred_len}.csv"), "w") as f:
 # with open(os.path.join("logs", f"Chronos_Tiny_CenBothUnbalanced_ICG_{context_len}_{pred_len}.csv"), "w") as f:
+# with open(os.path.join("logs", f"Chronos_Tiny_FA_IID_9_{context_len}_{pred_len}.csv"), "w") as f:
 
-with open(os.path.join("logs", f"Chronos_Tiny_FA_IID_9_{context_len}_{pred_len}.csv"), "w") as f:
+with open(os.path.join("logs", f"Chronos_Tiny_Scaffold_IID_9_{context_len}_{pred_len}.csv"), "w") as f:
     f.write("context_len,horizon_len,MSE,RMSE,MAE,SMAPE\n")
     for p_len in range(1, pred_len + 1):
         f.write(f"{context_len},{p_len},{mse_by_pred_len[p_len]},{rmse_by_pred_len[p_len]},{mae_by_pred_len[p_len]},{smapes_by_pred_len[p_len]}")
