3729f299a1a2734dfc271de97f34378697052af1
mahad852 <mahad852@gmail.com>
2025-01-20 06:44:14 -0500

    trying to make training on vital signs more efficient

--- custom_datasets/vital_signs_dataset.py
+++ custom_datasets/vital_signs_dataset.py
@@ -19,27 +19,26 @@ class VitalSignsDataset(Dataset):
                  is_train = True):
 
         self.root_folder = data_path        
+
+        self.is_train = is_train
+
         self.validate_path(self.root_folder, f"Incorrect data_path supplied. Expected a directory, {self.root_folder} is not a directory.")
         
         self.user_data_paths = self.get_user_data_paths(user_ids, scenarios)
+        self.data_lens = [len(self.get_data(user_data_path)) for user_data_path in self.user_data_paths]
+
+        self.start_indices = [0]
+        for i in range(1, len(self.data_lens)):
+            self.start_indices.append(self.start_indices[i - 1] + (self.data_lens[i - 1] - self.context_len - self.pred_len + 1))
 
         self.context_len = context_len
         self.pred_len = pred_len
 
         self.data_attribute = data_attribute
 
-        self.is_train = is_train
-        self.total_len = 0
-
-        self.data_file = None
-        self.data = None
-
-        self.set_indices()
-
-
     def __len__(self):
-        return self.total_len
-
+        return sum([data_len - self.pred_len - self.context_len + 1 for data_len in self.data_lens])
+        
     def get_user_data_paths(self, user_ids: List[str], scenarios: List[str]) -> List[str]:
         user_data_paths = []
         for user_id in user_ids:
@@ -63,53 +62,28 @@ class VitalSignsDataset(Dataset):
             raise ValueError(error_msg)
 
     def get_data(self, file):
-        if self.data_file != file:
-            self.clear_data()
+        data = scipy.io.loadmat(file)[self.data_attribute].reshape(-1)
 
-            self.data_file = file
-            self.data = scipy.io.loadmat(file)[self.data_attribute].reshape(-1)
-
-            total = self.data.shape[0]
-            if self.is_train:
-                self.data = self.data[:int(total * 0.60)]
-            else:
-                self.data = self.data[int(total * 0.60):]
-            
-            scaler = MinMaxScaler()
-            model = scaler.fit(self.data.reshape((-1, 1)))
-            self.data = model.transform(self.data.reshape((-1, 1))).reshape((-1))
-
-        return self.data
-    
-    def clear_data(self):
-        del self.data_file
-        self.data_file = None
+        total = self.data.shape[0]
+        if self.is_train:
+            data = data[:int(total * 0.60)]
+        else:
+            data = data[int(total * 0.60):]
         
-        del self.data
-        self.data = None
+        scaler = MinMaxScaler()
+        model = scaler.fit(data.reshape((-1, 1)))
+        data = model.transform(data.reshape((-1, 1))).reshape((-1))
+
+        return data
+    
         
     def __getitem__(self, index):
-        file = self.index_to_file[index]
-        file_index = index - self.file_to_start_index[file]
-        
-        data = self.get_data(file)
-        
-        return data[file_index : file_index + self.context_len], data[file_index + self.context_len : file_index + self.context_len + self.pred_len]
-
-    def set_indices(self):
-        self.index_to_file = {}
-        self.file_to_start_index = {}
-
-        self.total_len = 0
-        for file in self.user_data_paths:
-            data = self.get_data(file)
-
-            prev = self.total_len
-            self.total_len += (data.shape[0] - (self.context_len + self.pred_len) + 1)
+        file_index = len(self.start_indices)
+        for i in range(len(self.start_indices) - 1):
+            if index >= self.start_indices[i] and index < self.start_indices[i + 1]:
+                file_index = i
             
-            self.file_to_start_index[file] = prev
-            
-            for index in range(prev, self.total_len):
-                self.index_to_file[index] = file
-
-        self.clear_data()
+        data = self.get_data(self.user_data_paths[file_index])
+        start_index = index - self.start_indices[file_index]
+        
+        return data[start_index : start_index + self.context_len], data[start_index + self.context_len : start_index + self.context_len + self.pred_len]
